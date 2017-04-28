6a4ae9553b8730efae97b9b16445c5320361bd10
DESKTOP-1T3H7RT\rawhirokano <ramirogarciacano@gmail.com>
2017-04-29 00:37:00 +0100

    dqw

---
+++ esla-git-01/src/main/java/com/esla/experiment/git/esla_git_01/App.java
@@ -0,0 +1,13 @@
+package com.esla.experiment.git.esla_git_01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "A veeeeeeeeeeeeeeeeeeeeer" );
+    }
+}
